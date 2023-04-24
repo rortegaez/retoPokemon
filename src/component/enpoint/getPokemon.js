137528eff1d0c04cd594781f0310bbd30a150092
rortegaez <raulor83@gmail.com>
2023-04-24 19:59:22 +0200

    error https

--- src/component/enpoint/getPokemon.js
+++ src/component/enpoint/getPokemon.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const getPokemon = async (num) => {
-  const raw = await axios.get(`http://pokeapi.co/api/v2/pokemon/${num}`);
+  const raw = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
   return raw;
 };
 
