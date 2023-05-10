cd3f2203306ed65b8a7e1fead7f3086517843659
rortegaez <raulor83@gmail.com>
2023-05-10 02:24:59 +0200

    algofitmo fisher-yates sobre card

--- src/component/layout/KeyboardFind/index.js
+++ src/component/layout/KeyboardFind/index.js
@@ -1,40 +1,35 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
 
 //import constante
-import { SIZEFIND } from '../../constans.js'
+import { SIZEFIND } from "../../constans.js";
 
 //import components
-import ButtonRefresh from '../../buttons/ButtonRefresh/index.jsx'
-import fetchPokemon from '../../logic/fetchPokemon.js'
-import Card from '../../element/Card/index.js'
+import ButtonRefresh from "../../buttons/ButtonRefresh/index.jsx";
+import Card from "../../element/Card/index.js";
+import fetchPokFind from "../../logic/fetchPokFind.js";
 
 const KeyboardFind = () => {
+  const [list, setList] = useState([]);
 
-  const [list, setList ] = useState([])
+  const fetchPoke = async () => {
+    const pokes = await fetchPokFind(SIZEFIND);
+    setList(pokes);
+  };
 
-  const fetchPoke = async () =>{
-    const pokes = await fetchPokemon(SIZEFIND)
-    setList(pokes)
-  }
+  useEffect(() => {
+    fetchPoke();
+  }, []);
 
-  useEffect(() =>{
-    fetchPoke()
-  }, [])
-    
   return (
     <div>
-        <h1>KeyboardFind</h1>
-        {list.map((item, index) =>(
-          <Card
-          key={index}
-          id={index}
-          props={item.data}
-          />
-        ))}
-        <ButtonRefresh />
+      <h1>KeyboardFind</h1>
+      {list.map((item, index) => (
+        <Card key={index} id={index} props={item.data} />
+      ))}
+      <ButtonRefresh />
     </div>
-  )
-}
+  );
+};
 
-export default KeyboardFind
+export default KeyboardFind;
