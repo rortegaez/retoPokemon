bfc8167addc6d42233140bcfc4c656efa7f5e57a
rortegaez <raulor83@gmail.com>
2023-10-18 12:08:54 +0200

    feature smaller header

--- src/component/layout/Headers/HeaderWhats.js
+++ src/component/layout/Headers/HeaderWhats.js
@@ -4,8 +4,7 @@ import "../../../index.css";
 const HeaderWhats = () => {
   return (
     <div className="titleContainer">
-      <h1 className="titlePokemon">¿Cuál es el Pokemon?</h1>
-      <h2 className="subTitle">Push over Pokemon</h2>
+      <h1 className="titlePokemon">¿Cómo se llaman?</h1>
     </div>
   );
 };
